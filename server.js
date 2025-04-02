1a39970fad6eecfc7e1c88aa61034c9c919e8ef2
Hien Vo <aswind0101@gmail.com>
2025-04-02 00:23:31 -0700

    getCoinPrice from server.js

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+import priceRoute from './routes/price.js';
+app.use("/api/price", priceRoute);
+
+
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
